b554b257e498029f0f1abb323676c3e7436356e5
kevincar <kevincarrolldavis@gmail.com>
2018-03-19 20:59:27 -0500

    Begin Dictionary Translation testing

--- src/SheetObjectDictionary.ts
+++ src/SheetObjectDictionary.ts
@@ -1,20 +1,32 @@
-class SheetObjectDictionary<T extends SheetObjectConstructor, U extends SheetObjectInstance>{
+class SheetObjectDictionary<T extends SheetObject>{
 
+	ctor: SheetObjectConstructor<T> | null = null;
 	sheet: Sheet | null = null;
 
-	constructor(extendedSheetObject: T, sheet: Sheet) {
-		let temporaryObject = new extendedSheetObject();
+	constructor(ctor: SheetObjectConstructor<T>, sheet: Sheet) {
+		this.ctor = ctor;
 		this.sheet = sheet;
 	}
 
 	[property: string]: any;
 
-	//translate(): U[] {
-		//sheet.values.forEach((rowData: any[], rowIndex: number) => {
-			//if(rowIndex == 0) return;
+	translate(): T[] {
+		if(!this.sheet) throw `Sheet is undefined!`;
+		return this.sheet.values.map((rowData: any[], rowIndex: number): T => {
+			if(this.ctor == null) throw `undefined constructor`;
+			if(!this.sheet) throw `Sheet is undefined!`;
+			if(rowIndex == 0) return new this.ctor({});
 
-			//let data: any = {};
-	
-		//}); 
-	//} 
+			let headers: any[] = this.sheet.headers;
+			let data: any = {};
+
+			headers.forEach((header: any, index: number) => {
+				data[header] = rowData[index];
+			});
+
+			let instance: T = new this.ctor(data);
+			return instance;
+
+		}); 
+	} 
 }
--- src/tests/SheetObjectDictionary.spec.ts
+++ src/tests/SheetObjectDictionary.spec.ts
@@ -1,4 +1,12 @@
 function SheetObjectDictionaryTap(tap: GasTap): void {
 
+	let ss: Spreadsheet = new Spreadsheet();
+	let sheet: Sheet = new Sheet(ss, "Sheet1");
+	let sod = new SheetObjectDictionary(MouseObject, sheet);
+
+	tap.test("Translation testing", (t: test) => {
+		
+	});
+
 	return;
 }
