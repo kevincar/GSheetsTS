10d7fed94afbae967d6715915910c5f3230cbda3
kevincar <kevincarrolldavis@gmail.com>
2018-04-28 08:19:28 -0500

    Added conversion for Google Sheets Date Times

--- src/SheetObject.ts
+++ src/SheetObject.ts
@@ -10,6 +10,7 @@ interface SheetObjectConstructor<T extends SheetObject> {
 }
 
 abstract class SheetObject implements SheetObjectInstance {
+	gDateConversion: number = 2209143600000;
 	getData(): SheetObjectInterface {
 		return new Array();
 	}
@@ -22,5 +23,29 @@ abstract class SheetObject implements SheetObjectInstance {
 		}, true);
 		
 		return !allValuesBad;
+	}
+
+	convertFromGDate(dateValue: string | number | null): Date | null {
+		if(dateValue == null) return dateValue;
+
+		if(typeof(dateValue) == 'string') {
+			dateValue = parseInt(dateValue);
+		}
+
+		if(typeof(dateValue) != 'number' || isNaN(dateValue))
+			throw `dateValue: ${dateValue} is not a string nor a number!`;
+
+		let gTime: number = dateValue * 24 * 3600 * 1000;
+		let gDate: Date = new Date(gTime);
+		let convertedTime: number = gDate.getTime() - this.gDateConversion;
+		return new Date(convertedTime);
+	}
+
+	convertToGDate(date: Date | null): number | null {
+		if(date == null) return date;
+		let convertedTime: number = date.getTime();
+		let gTime: number = convertedTime + this.gDateConversion;
+		let dateValue: number = gTime / (24 * 3600 * 1000);
+		return dateValue;
 	}
 }
