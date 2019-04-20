d7e9bb35ca7a856294c49a19c508a18b82202cd1
Kevin Davis <kevincarrolldavis@gmail.com>
2019-04-20 00:25:52 -0400

    Added new specs for the Spreadsheet

--- tests/src/Spreadsheet.spec.ts
+++ tests/src/Spreadsheet.spec.ts
@@ -6,4 +6,17 @@ function spreadsheetTest(tap: GasTap):void {
 		t.equal(observed, expected, "names are equal");
 	});
 
+	tap.test("Spreadsheet should be able to create and delete a sheet", (t: test): void => {
+		let sheetName: string = "testSheet";
+		let ss: Spreadsheet = new Spreadsheet();
+		ss.createSheet(sheetName);
+		
+		let sheetFound: boolean = ss.sheetNames.filter((e) => {return e == sheetName;}).length > 0;
+		t.equal(sheetFound, true, "sheet created");
+
+		ss.deleteSheet(sheetName);
+		sheetFound = ss.sheetNames.filter((e) => {return e == sheetName;}).length > 0;
+		t.notEqual(sheetFound, false, "sheet deleted");
+	});
+
 }
